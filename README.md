4028b760d1319497adfb67eefd1ab0188eea9b16
Aung Myo Kyaw <me@aungmyokyaw.com>
2016-10-13 12:22:37 +0000

    Requrest Header Parser

---
+++ README.md
@@ -0,0 +1,12 @@
+# Request Header Parser
+
+## Example Usage
+- https://request-header-parser-aungmyokyaw.c9users.io/api/whoami
+
+## Example Output
+{"ipaddress":"x.x.x.x","language":"en-US","software":"X11; Linux i686"}
+
+## Installation
+```
+npm start
+````
---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+app.use('/',express.static('public'));
+app.get('/api/whoami',function(req,res){
+    var software = req.headers['user-agent'].split(' ');
+    var data = {
+        ipaddress: req.headers['x-forwarded-for'],
+        language: req.headers['accept-language'].split(',')[0],
+        software: software[1].slice(1)+" "+software[2]+" "+software[3].slice(0,-1)
+    }
+    res.json(data);
+});
+app.get('/*',function(req,res){
+    var data = '//'+req.hostname+'/api/whoami'
+    res.redirect(data);
+});
+app.listen(8080);
